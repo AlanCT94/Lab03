0c5641b5218a240f0963d84a00535ffb7187d891
AlanCT94 <caciqueta94@gmail.com>
2022-09-13 17:03:02 +0200

    .R documents commented with Roxygen

--- R/euclidean.R
+++ R/euclidean.R
@@ -1,3 +1,12 @@
+#' Euclidean Algorithm implementation
+#'
+#' @param a integer
+#' @param b integer
+#'
+#' @return a
+#' @export
+#'
+#' @examples euclidean(100, 1000)
 euclidean <- function(a,b){
   while (a != b) {
     if (a > b){
