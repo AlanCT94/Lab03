0169c36f9020262ea0bf6f4e9fd83ca58443b24b
AlanCT94 <caciqueta94@gmail.com>
2022-09-14 11:39:18 +0200

    changes

--- R/euclidean.R
+++ R/euclidean.R
@@ -3,19 +3,20 @@
 #' @param a integer
 #' @param b integer
 #'
-#' @return a
+#' @return greatest common divisor
 #' @export
 #'
 #' @examples euclidean(100, 1000)
 euclidean <- function(a,b){
   stopifnot(is.numeric(a),length(a)== 1)
   stopifnot(is.numeric(b),length(a)== 1)
+
   while (a != b) {
     if (a > b){
       a <- a-b
     }
     else{
-      b <- b - a
+      b <- b-a
     }
   }
   return(a)
